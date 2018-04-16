3a8e3d44c06646d2e42aba2530aa7a445b6d5a17
AswinPeiris <u6177012@anu.edu.au>
2018-04-16 17:54:02 +1000

    switched over to intelliJ, which created some new files

--- src/webcrawler.java
+++ src/webcrawler.java
@@ -13,17 +13,16 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
-public static int pages = 11;
-public static int port  = 3310;
-
 public class webcrawler{
+    static final int pages = 11;
+    //static final int port  = 3310;
     int visitedPages    =  0;
     String largestPage  = "";
     String recentPage   = "";
     String PgDateTime   = "";
-    ArrayList invalidPgs = new ArrayList();
-    String address = "http://3310exp.hopto.org:9780/";
-    Scanner scan = new Scanner(System.in);
+    ArrayList VstDPgs = new ArrayList();
+    ArrayList InvldPgs = new ArrayList();
+    //static final String address = "http://3310exp.hopto.org:9780/";
 
     public static void main (String[] args) throws IOException{
       crawler();
@@ -31,18 +30,42 @@ public class webcrawler{
 
     public static void crawler(){
       try{
-        URL site =  new URL (address);
-        //URLConnection c = oracle.openConnection();
-        BufferedReader in = new BufferedReader(new InputStreamReader (site.openStream()));
+        //get user input
+          Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter the website address: ");
+        String address = scan.next();
+        System.out.println("Please enter the port: ");
+        int port = scan.nextInt();
 
-        String inputLine;
-        while((inputLine = in.readLine()) != null){
-          System.out.println(inputLine);
-        }
-        in.close();
+        //open socket connection to server
+          //this code was mostly adapted from the second tutorial client/server code
+        Socket sock =  new Socket(address,port);
+        System.out.println("Webcrawler: created socket connected to local port" +
+                sock.getLocalPort() + "and to remote address" +
+                sock.getInetAddress() + "and port" +
+                sock.getPort());
+        //read through pages
+
+        //return results
+
+        //close socket
+        sock.close();
+        //URL site =  new URL (address)
+        //URLConnection c = oracle.openConnection();
       }
       catch (IOException e){
-        System.out.println("ERROR: cannot connect to website");
+        System.out.println("ERROR: cannot connect to the website");
       }
+    }
+
+    public String pageReader(String address){
+      return null;
+    }
+
+    public String urlVerification(String address){
+      String verification = "yes";
+      //checks if we have visited the webpage previously
+      //checks if it was listed as invalid
+      return verification;
     }
 }
