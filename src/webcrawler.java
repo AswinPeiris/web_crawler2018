e70d8d9ff65feb5f417d37f5d02d9c85080e5741
AswinPeiris <u6177012@anu.edu.au>
2018-04-23 14:16:55 +1000

    backingup

--- src/webcrawler.java
+++ src/webcrawler.java
@@ -35,8 +35,9 @@ public class webcrawler{
         //get user input for the URL and Port
         Scanner scan = new Scanner(System.in);
         System.out.println("Please enter the website address (don't include the 'http://' in front): ");
-        String address = "http://"+scan.next();
-        VisitedPgs.add(address);
+        String htt = "http://";
+        String address = scan.next();
+        address = htt+address;
         URL url = new URL(address);
         String path = url.getPath();
         System.out.println("Please enter the port: ");
@@ -47,13 +48,13 @@ public class webcrawler{
         int port = scan.nextInt();
         //open socket connection and states details about the connection
         Socket sock = new Socket (url.getHost(), port);
-        System.out.println();
         System.out.println("Webcrawler: socket connected to server's local port: " + sock.getLocalPort());
         System.out.println("Remote address: " + sock.getInetAddress());
         System.out.println("Port: " + sock.getPort());
         System.out.println();
         //System.out.println("Path: "+ path);
         //get request for html page from server
+        System.out.println("...");
         OutputStream out = sock.getOutputStream();
         PrintWriter printwriter = new PrintWriter(out, false);
         printwriter.print("GET "+path+" HTTP/1.0\r\n");
@@ -64,14 +65,14 @@ public class webcrawler{
         InputStreamReader InSR = new InputStreamReader(in);
         BufferedReader buff = new BufferedReader(InSR);
           String line;
+          System.out.println("...");
+          VisitedPgs.add(address);
         while((line = buff.readLine()) != null){
-            System.out.println(line);
             if(line.contains("HTTP/1.1 4")){
                 InvldPgs.add(address);
                 VisitedPgs.add(address);
                 break;
             }else if(line.contains("HTTP/1.1 3")){
-                VisitedPgs.add(address);
                 while((line = buff.readLine()) != null){
                     if(line.contains("<a href=")){
                         String redirect = line.substring(line.lastIndexOf("href=")+6,line.lastIndexOf('"'));
@@ -105,24 +106,26 @@ public class webcrawler{
             }
         }
         System.out.println();
+        //closing the socket and other imports
         printwriter.close();
         out.close();
         in.close();
         scan.close();
         sock.close();
+        System.out.println("This may take a while...");
+        //reads through the rest of the pages
         pageReader(pages.get(1), port);
-        //System.out.println(pages);
-        //System.out.println(VisitedPgs);
-        //read through pages
+        System.out.println("...");
         //return results
-        //close socket
-
         System.out.println("------------------------------------------------------");
         System.out.println("------------------------REPORT------------------------");
-        System.out.println("Total number of URLs visited: "+VisitedPgs.size()+" and: "+VisitedPgs);
+        System.out.println("Total number of URLs visited: "+VisitedPgs.size()+" and :");
+        for(int i = 0; i < VisitedPgs.size(); i++){
+          System.out.println(VisitedPgs.get(i));
+        }
         System.out.println("Total number of Invalid URLs: "+InvldPgs.size());
         System.out.println("The largest page: "+largestPage+" and size: "+largestPageLength);
-        System.out.println("The most recently modifed page date/time: "+ recentPage);
+        System.out.println("The most recently modifed page: "+ recentPage+" and its date/time: "+PgDateTime);
         System.out.println("List of redirected pages: ");
         for(int i = 0; i < reDirPgs.size();i++) {
             System.out.println(reDirPgs.get(i));
@@ -139,13 +142,13 @@ public class webcrawler{
 
     private static void pageReader(String address, int port) throws Exception{
         try{
-            TimeUnit.SECONDS.sleep(4);
-            VisitedPgs.add(address);
+            System.out.println("adding slight delay!");
+            TimeUnit.SECONDS.sleep(2);
             URL url = new URL (address);
             String path = url.getPath();
             if(path.isEmpty()){
               path = "/index.html";
-          }
+            }
             //initiate socket connection
             Socket sock = new Socket(url.getHost(),port);
             //begins procedure to send get request to server
@@ -159,16 +162,21 @@ public class webcrawler{
             InputStreamReader InSR = new InputStreamReader(in);
             BufferedReader buff = new BufferedReader(InSR);
             String line;
+            VisitedPgs.add(address);
             while((line = buff.readLine()) != null){
               if(line.contains("HTTP/1.1 4")){
+                  //getting 4xx responses
                   InvldPgs.add(address);
                   pages.remove(address);
                   break;
               }else if(line.contains("HTTP/1.1 3")){
-                 VisitedPgs.add(address);
                  while((line = buff.readLine())!= null){
+                   //getting 3xx responses
                    if(line.contains("<a href=")){
                      String redirect = line.substring(line.lastIndexOf("href=")+6, line.lastIndexOf('"'));
+                     if(urlVerification(redirect) == false){
+                       pages.add(redirect);
+                     }
                      reDirPgs.add(address+" and the destination:"+redirect);
                      break;
                    }
@@ -176,6 +184,7 @@ public class webcrawler{
                  break;
               }else {
                   if (line.contains("Content-Length: ")) {
+                      //getting the size of page
                       String contentLength = line.substring(16);
                       int cl = Integer.parseInt(contentLength);
                       if (cl >= largestPageLength) {
@@ -184,6 +193,7 @@ public class webcrawler{
                       }
                   }
                   if (line.contains("Last-Modified: ")) {
+                      //this section will get most recently modified date
                       String lastMod = line.substring(15);
                       //newest date
                       DateFormat lm = new SimpleDateFormat("E, dd MMM YYYY HH:mm:ss z", Locale.ENGLISH);
@@ -197,6 +207,7 @@ public class webcrawler{
                       }
                   }
                   if(line.contains("<a href=")) {
+                      //extracting the links
                       String gatheredLink = line.substring(line.lastIndexOf("href=")+6,line.lastIndexOf('"'));
                       if(urlVerification(gatheredLink) == true){
                         continue;
@@ -220,6 +231,9 @@ public class webcrawler{
           System.out.println("ERROR: Something is wrong with your URL");
         }catch (IOException e){
           System.out.println("ERROR: cannot connect to the website");
+          //adds a problem webpage to the invalids and visited to avoid visiting it again.
+          VisitedPgs.add(address);
+          InvldPgs.add(address);
         }
     }
 
